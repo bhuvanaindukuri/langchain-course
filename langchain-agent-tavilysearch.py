098ed792496f1616bd0ea99bfa6dd4c8b7783f08
Bhuvaneswari <bhuvana.nitw@gmail.com>
2026-03-19 21:07:29 +0530

    Langchain agent using Tavily search

---
+++ langchain-agent-tavilysearch.py
@@ -0,0 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
+from langchain_core.tools import tool
+from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+
+llm = ChatOllama(model="qwen2.5:0.5b", temperature=0)
+tools = [TavilySearch()]
+agent = create_agent(
+    model=llm,
+    tools=tools,
+    system_prompt="You are a helpful assistant. Use tools when needed.",
+)
+
+
+def main():
+    print("Hello from langchain agent")
+    result = agent.invoke(
+        {"messages": [HumanMessage(content="Search for Chief Architect positions in LinkedIn in Bangalore")]}
+    )
+    print(result["messages"][-1].content)
+    
+
+
+if __name__ == "__main__":
+    main()
